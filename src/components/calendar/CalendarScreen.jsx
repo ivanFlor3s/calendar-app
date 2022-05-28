5cb711c7a7d441048edcfce7bef47ad6565fc1e9
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-27 23:51:10 -0300

    Evento ejemplo en calendario

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,34 +1,32 @@
-import { Calendar, luxonLocalizer} from 'react-big-calendar'
-import { DateTime} from 'luxon'
-import { Navbar } from '../ui/Navbar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar, luxonLocalizer } from "react-big-calendar";
+import { DateTime } from "luxon";
+import { Navbar } from "../ui/Navbar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const localizer = luxonLocalizer(DateTime,{firstDayOfWeek: 7})
+const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 7 });
 const events = [
-   { 
-       title:'Dentista, me rompen la boca',
-       start: DateTime.now(),
-       end: DateTime.now().plus({hours:2}),
-       bgcolor: '#fafafa'
-    }
-    
-]
+  {
+    title: "Dentista, me rompen la boca",
+    start: DateTime.now().minus({hour:4}).toJSDate(),
+    end: DateTime.now().minus({hour:2}).toJSDate(),
+    // start:new Date('2022-05-28T01:36:42.486-03:00'),
+    // end:new Date('2022-05-29T01:36:42.486-03:00',
+    bgcolor: "#fafafa",
+  },
+];
 
 export const CalendarScreen = () => {
+  console.log(DateTime.now().toJSDate());
   return (
-      <div className='calendar-screen'>
-          <Navbar></Navbar>
+    <div className="calendar-screen">
+      <Navbar></Navbar>
 
-          <Calendar
-            localizer = {localizer}
-            events = {events}
-
-            startAccesor = "start"
-            endAccessor="end"
-          >
-
-          </Calendar>
-
-      </div>
-  )
-}
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccesor="start"
+        endAccessor="end"
+      ></Calendar>
+    </div>
+  );
+};
