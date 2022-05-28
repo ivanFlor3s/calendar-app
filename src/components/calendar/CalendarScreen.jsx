d12ae02e867d5c659452f277956f0453beb83f0c
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-27 23:28:59 -0300

    React big calendar

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Calendar, luxonLocalizer} from 'react-big-calendar'
+import { DateTime} from 'luxon'
 import { Navbar } from '../ui/Navbar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = luxonLocalizer(DateTime,{firstDayOfWeek: 7})
+const events = [
+   { 
+       title:'Dentista, me rompen la boca',
+       start: DateTime.now(),
+       end: DateTime.now().plus({hours:2}),
+       bgcolor: '#fafafa'
+    }
+    
+]
 
 export const CalendarScreen = () => {
   return (
-    <Navbar></Navbar>
+      <div className='calendar-screen'>
+          <Navbar></Navbar>
+
+          <Calendar
+            localizer = {localizer}
+            events = {events}
+
+            startAccesor = "start"
+            endAccessor="end"
+          >
+
+          </Calendar>
+
+      </div>
   )
 }
