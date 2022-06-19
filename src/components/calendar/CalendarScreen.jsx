17a68f3b3686995a075a2042e7c51135e35380ad
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-06-18 21:10:26 -0300

    Eventos calendarios

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -2,27 +2,38 @@ import { Calendar, luxonLocalizer } from "react-big-calendar";
 import { DateTime } from "luxon";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { CalendarEvent, Navbar } from "../";
+import { useState } from "react";
 
 const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 7 });
-
 
 const events = [
   {
     title: "Dentista, me rompen la boca",
-    start: DateTime.now().minus({hour:4}).toJSDate(),
-    end: DateTime.now().minus({hour:2}).toJSDate(),
+    start: DateTime.now().minus({ hour: 4 }).toJSDate(),
+    end: DateTime.now().minus({ hour: 2 }).toJSDate(),
     // start:new Date('2022-05-28T01:36:42.486-03:00'),
     // end:new Date('2022-05-29T01:36:42.486-03:00',
     bgcolor: "#fafafa",
     user: {
-      name: 'felipee2do'
-    }
+      name: "felipee2do",
+    },
   },
 ];
 
 export const CalendarScreen = () => {
 
+  const [lastView] = useState(localStorage.getItem('lastView') || 'week')
 
+  const onDoubleClick = (event) => {
+    console.log({ doubleClick: event });
+  };
+  const onSelect = (event) => {
+    console.log({ lick: event });
+  };
+  const onViewChanged = (event) => {
+    console.log({ onViewChanged: event });
+    localStorage.setItem('lastView', event)
+  };
   return (
     <div className="calendar-screen">
       <Navbar></Navbar>
@@ -30,12 +41,16 @@ export const CalendarScreen = () => {
       <Calendar
         localizer={localizer}
         events={events}
-        style={{height:'calc(100vh - 80px)'}}
+        defaultView={lastView}
+        style={{ height: "calc(100vh - 80px)" }}
         startAccesor="start"
         endAccessor="end"
         components={{
-          event: CalendarEvent
+          event: CalendarEvent,
         }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView = {onViewChanged}
       ></Calendar>
     </div>
   );
